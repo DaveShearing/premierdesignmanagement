838f2a1a803323b9864f4c62586893e398229873
Dave Shearing <electrical.madness@hotmail.co.uk>
2019-09-09 23:05:23 +0100

    initil

---
+++ PremierDesignManagement/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PremierDesignManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+
+        private void TaskListButtonClick(object sender, RoutedEventArgs e)
+        {
+            TaskListBorder.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFFF4F5A");
+            CalendarBorder.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF51545D");
+            CalendarGrid.Visibility = Visibility.Hidden;
+            TaskListGrid.Visibility = Visibility.Visible;
+        }
+
+        private void CalendarButtonClick(object sender, RoutedEventArgs e)
+        {
+            CalendarBorder.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFFF4F5A");
+            TaskListBorder.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF51545D");
+            TaskListGrid.Visibility = Visibility.Hidden;
+            CalendarGrid.Visibility = Visibility.Visible;
+        }
+
+    }
+}
